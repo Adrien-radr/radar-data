63f789492e273c79b0258734cce7fc54ae81a6e1
Adrien-radr <ra.dubouchet@gmail.com>
2018-10-25 16:49:10 -0400

    adaptive camera-distance based tesselation

--- shaders/planet_tesc.glsl
+++ shaders/planet_tesc.glsl
@@ -5,16 +5,29 @@ layout (vertices=3) out;
 in vec3 vPosition[];
 out vec3 tcPosition[];
 
+uniform vec3 CameraPosition;
+
+float GetTesselationLevel(float dist0, float dist1)
+{
+	float avgDist = (dist0 + dist1) / 2.0;
+	return max(1.0, min(32.0, 32.0 / sqrt(avgDist)));
+}
+
 void main()
 {
 	tcPosition[gl_InvocationID] = vPosition[gl_InvocationID];
 
-	if(gl_InvocationID == 0)
+	//if(gl_InvocationID == 0)
 	{
-		gl_TessLevelInner[0] = 8.0;
+		float vDist0 = distance(CameraPosition, tcPosition[0]);
+		float vDist1 = distance(CameraPosition, tcPosition[1]);
+		float vDist2 = distance(CameraPosition, tcPosition[2]);
 
-		gl_TessLevelOuter[0] = 8.0;
-		gl_TessLevelOuter[1] = 8.0;
-		gl_TessLevelOuter[2] = 8.0;
+
+		gl_TessLevelOuter[0] = GetTesselationLevel(vDist1, vDist2);
+		gl_TessLevelOuter[1] = GetTesselationLevel(vDist0, vDist2);
+		gl_TessLevelOuter[2] = GetTesselationLevel(vDist0, vDist1);
+		
+		gl_TessLevelInner[0] = gl_TessLevelOuter[2];
 	}
 }
--- shaders/planet_vert.glsl
+++ shaders/planet_vert.glsl
@@ -4,8 +4,10 @@ layout (location=0) in vec3 position;
 
 out vec3 vPosition;
 
+uniform mat4 ModelMatrix;
+
 void main()
 {
-	vPosition = position;
-	gl_Position = vec4(position, 1.0f);
+	vPosition = (ModelMatrix * vec4(position, 1.0)).xyz;
+	//gl_Position = vec4(position, 1.0f);
 }
